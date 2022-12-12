9775a0a37550699906d778513b65965ddffc745b
innie <innie0526@gmail.com>
2022-12-13 04:55:20 +0900

    fix : css 수정

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -20,20 +20,34 @@ useEffect(() => {
 
 
     return (
+        <AuthBox>
+
         <Container>
             {isLoggedin ? <SignIn/> : <SignUp/>}
             <Button
             className="d-flex mx-auto mt-3"
-            variant="outline-info"
+            variant="outline-danger"
             size="sm"
             onClick={changePage}
-        >
-          {isLoggedin ? '회원가입 하러가기' : '로그인으로 돌아가기'}
+            >
+        {isLoggedin ? '회원가입 하러가기' : '로그인으로 돌아가기'}
         </Button>
         </Container>
-
+            </AuthBox>
     );
 };
 
 export default Auth;
 
+const AuthBox = styled.div`
+position: absolute;
+background: #ffff89;
+border:1px solid #000000;
+top:50%;
+left:50%;
+transform: translate(-50%, -50%);
+width: 500px;
+margin:0 auto;
+padding: 40px;
+box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1);
+`
