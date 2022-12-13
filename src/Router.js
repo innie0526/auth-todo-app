d955a5960e11c6d5037845c5ff03c7be8fe0eabc
innie <innie0526@gmail.com>
2022-12-14 06:59:10 +0900

    fix: router.js 수정

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/Auth/SignUp";
 import Todo from "./pages/Todo/Todo";
 import Auth from "./pages/Auth/Auth";
 
 const App =()=> {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/"  element={<Auth/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/todo" element={<Todo/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
