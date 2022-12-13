eca5f4bdf5721a93ed52799a83ca8841d86c5202
innie <innie0526@gmail.com>
2022-12-14 03:18:55 +0900

    fix: 코드 정리

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import 'bootstrap/dist/css/bootstrap.min.css' ;
-import { Provider } from 'react-redux';
-import { createStore } from '@reduxjs/toolkit';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducer, composeWithDevTools());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
-    <Provider store={store}>
+    <>
     <Router />
-    </Provider>
+    </>
 );
